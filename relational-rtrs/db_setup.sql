1b02b027a4c7bd7bff621269f0b0a8e9fb001b81
Anum Saghir <anumsaghir@gmail.com>
2017-11-25 01:01:44 +0500

    all updates done

--- relational-rtrs/db_setup.sql
+++ relational-rtrs/db_setup.sql
@@ -4,7 +4,7 @@ create table movies(
     title varchar(500) not null,
     genres varchar(300)
 );
-DROP TABLE IF EXITS movies;
+DROP TABLE IF EXITS tags;
 create table tags(
     user_id int not null, 
     movie_id int not null,
@@ -12,25 +12,25 @@ create table tags(
     timestamp int unsigned not null,
     PRIMARY KEY(user_id, movie_id)
 );
-DROP TABLE IF EXITS movies;
+DROP TABLE IF EXITS links;
 create table links(
     movie_id int unsigned not null primary key,
     imdbid int not null,
     tmdbid int not null
 );
-DROP TABLE IF EXITS movies;
+DROP TABLE IF EXITS genome_scores;
 create table genome_scores(
     movie_id int not null,
     tag_id int not null,
     relevance float not null,
     PRIMARY KEY(movie_id, tag_id)
 );
-DROP TABLE IF EXITS movies;
+DROP TABLE IF EXITS genome_tags;
 create table genome_tags(
     tag_id int not null primary key,
     tag varchar(500) not null
 );
-DROP TABLE IF EXITS movies;
+DROP TABLE IF EXITS rating;
 create table ratings(
     user_id int not null, 
     movie_id int not null, 

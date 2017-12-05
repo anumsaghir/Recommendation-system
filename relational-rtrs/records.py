be0eba4ca53bf0ee1e52fcabd4a6099189bf2ca3
Kashif Iftikhar <kashif@compulife.com.pk>
2017-12-06 00:49:54 +0500

    recursive loop

--- relational-rtrs/records.py
+++ relational-rtrs/records.py
@@ -4,6 +4,13 @@ To Fatch Records
 
 from models import Movie, get_db 
 db= get_db()
-movies= db.query(Movie).limit(10)
-for movie in movies:             
+movies= db.query(Movie).limit(10).all()
+i = 0
+
+for movie in movies:
     print(movie.movie_id)
+    i += 1
+    
+    for second_movie in movies[i:]:
+        print("    %i" % second_movie.movie_id)
+

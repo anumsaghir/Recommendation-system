10770ecd093a0794f8854cf0332afaf3deededae
Anum Saghir <anumsaghir@gmail.com>
2017-12-06 01:01:01 +0500

    all updates done

--- relational-rtrs/records.py
+++ relational-rtrs/records.py
@@ -4,6 +4,6 @@ To Fatch Records
 
 from models import Movie, get_db 
 db= get_db()
-movies= db.query(Movie).limit(10)
+movies= db.query(Movie).limit(20)
 for movie in movies:             
     print(movie.movie_id)

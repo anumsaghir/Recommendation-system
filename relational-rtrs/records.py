175d6e9712c0896704f6b6cc189daba6aa9c6b3b
Anum Saghir <anumsaghir@gmail.com>
2017-12-06 01:27:11 +0500

    all updates done

--- relational-rtrs/records.py
+++ relational-rtrs/records.py
@@ -4,18 +4,13 @@ To Fatch Records
 
 from models import Movie, get_db 
 db= get_db()
-<<<<<<< HEAD
-movies= db.query(Movie).limit(10)
-for movie in movies:             
-=======
 movies= db.query(Movie).limit(10).all()
 i = 0
 
 for movie in movies:
->>>>>>> be0eba4ca53bf0ee1e52fcabd4a6099189bf2ca3
     print(movie.movie_id)
     i += 1
     
     for second_movie in movies[i:]:
-        print("    %i" % second_movie.movie_id)
+print("    %i" % second_movie.movie_id))
 

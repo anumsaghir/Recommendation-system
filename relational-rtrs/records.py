f6a746ab63e02fcd846765bccdd7fc628ea88e5d
Anum Saghir <anumsaghir@gmail.com>
2017-12-06 03:27:53 +0500

    all updates done

--- relational-rtrs/records.py
+++ relational-rtrs/records.py
@@ -3,7 +3,9 @@
 To Fatch Records
 """
 
-from models import Movie, get_db 
+from models import Movie, get_db
+from similarity import jaccard_index 
+from models import Similarity
 db= get_db()
 movies= db.query(Movie).limit(10).all()
 i = 0
@@ -14,4 +16,14 @@ for movie in movies:
     for second_movie in movies[i:]:
         print(movie.title)
         print("    %s" % second_movie.title)
+        from similarity import jaccard_index
+        s_val = jaccard_index(movie.title, second_movie.title)
+        print(s_val)
+        from models import Similarity
+        r=Similarity()
+        r.movie_id_1=movie.movie_id
+        r.movie_id_2=second_movie.movie_id
+        r.similarity_index=s_val
+        db.add(r)
+        db.commit()
 

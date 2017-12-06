7dfbb9dd48d9e42e0dd666113a7f2543579b463b
Anum Saghir <anumsaghir@gmail.com>
2017-12-06 23:44:28 +0500

    all updates done

--- relational-rtrs/records.py
+++ relational-rtrs/records.py
@@ -7,7 +7,7 @@ from models import Movie, get_db
 from similarity import jaccard_index 
 from models import Similarity
 db= get_db()
-movies= db.query(Movie).limit(10).all()
+movies= db.query(Movie).all()
 i = 0
 
 for movie in movies:
@@ -16,10 +16,8 @@ for movie in movies:
     for second_movie in movies[i:]:
         print(movie.title)
         print("    %s" % second_movie.title)
-        from similarity import jaccard_index
         s_val = jaccard_index(movie.title, second_movie.title)
         print(s_val)
-        from models import Similarity
         r=Similarity()
         r.movie_id_1=movie.movie_id
         r.movie_id_2=second_movie.movie_id

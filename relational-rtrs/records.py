9bf3797070ec39c0f086fdb77eaf6cab918f9ba8
Kashif Iftikhar <kashif@compulife.com.pk>
2017-12-08 01:05:50 +0500

    Optimized mysql import

--- relational-rtrs/records.py
+++ relational-rtrs/records.py
@@ -3,25 +3,35 @@
 To Fatch Records
 """
 
-from models import Movie, get_db
-from similarity import jaccard_index 
-from models import Similarity
-db= get_db()
-movies= db.query(Movie).all()
+from models import Movie, Similarity, get_db
+from similarity import jaccard_index
+
+db = get_db()
+movies = db.query(Movie).all()
+
 i = 0
+rec_count = 0
 
 for movie in movies:
     i += 1
-    
-    for second_movie in movies[i:]:
-        print(movie.title)
-        print("    %s" % second_movie.title)
-        s_val = jaccard_index(movie.title, second_movie.title)
-        print(s_val)
-        r=Similarity()
-        r.movie_id_1=movie.movie_id
-        r.movie_id_2=second_movie.movie_id
-        r.similarity_index=s_val
-        db.add(r)
-        db.commit()
+    print(movie.movie_id)
 
+    rec_count = 0
+    for second_movie in movies[i:]:
+        # print(movie.title)
+        # print("    %s" % second_movie.title)
+        s_val = jaccard_index(movie.title, second_movie.title)
+        # print(s_val)
+        r = Similarity()
+        r.movie_id_1 = movie.movie_id
+        r.movie_id_2 = second_movie.movie_id
+        r.similarity_index = s_val
+        db.add(r)
+
+        rec_count += 1
+        # insert records in a batch of 100 per commit to speed up insertions
+        if rec_count % 100 == 0:
+            db.commit()
+
+    if rec_count % 100 != 0:
+        db.commit()

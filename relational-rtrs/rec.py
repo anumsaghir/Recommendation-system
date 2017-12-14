a9b0f331190cccd41cd168d073cf07394011e7d5
Anum Saghir <anumsaghir@gmail.com>
2017-12-15 00:18:33 +0500

    all updates done

--- relational-rtrs/rec.py
+++ relational-rtrs/rec.py
@@ -1,15 +1,12 @@
-from models import Movie,get_db
+from models import get_db
 db= get_db()
-res=db.execute('select movie_id, title from movies')
+res=db.execute('select * from similarities where movie_id_1=111351 or movie_id_2=111351 order by similarity_index desc limit 10')
 i = 0
-rec_count = 0
+rec_count = 10
 for r in res:
     i += 1
-    print(movie.title)
-    for second_movie in res[i:]:
-         print(movie.title)
-         print("    %s" % second_movie.title)
-
+    print(r[2])
+   
 
 
 

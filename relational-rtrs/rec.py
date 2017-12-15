0a7952a30329d4a6ea83115efa8fd43f59bb2571
Kashif Iftikhar <kashif@compulife.com.pk>
2017-12-15 22:37:50 +0500

    movie id as command line argument

--- relational-rtrs/rec.py
+++ relational-rtrs/rec.py
@@ -1,6 +1,15 @@
+import sys
 from models import get_db
+
+if len(sys.argv) != 2:
+    print("Usage %s movie_id" % sys.argv[0])
+    sys.exit(1)
+
+movie_id = int(sys.argv[1])
+
 db= get_db()
-res=db.execute('select * from similarities where movie_id_1=111351 or movie_id_2=111351 order by similarity_index desc limit 10')
+res=db.execute('select * from similarities where movie_id_1=%i or movie_id_2=%i order by similarity_index desc limit 10' % (movie_id, movie_id))
+
 i = 0
 rec_count = 10
 for r in res:
@@ -9,10 +18,4 @@ for r in res:
         print(r[0],r[2])
     else:
         print(r[1],r[2])
-    
-    
-   
-
-
-
 

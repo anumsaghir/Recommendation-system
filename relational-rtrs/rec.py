b1d71d81b06094a316623ef38a36acc686503981
Anum Saghir <anumsaghir@gmail.com>
2017-12-17 22:57:10 +0500

    all updates done

--- relational-rtrs/rec.py
+++ relational-rtrs/rec.py
@@ -1,5 +1,5 @@
 import sys
-from models import get_db
+from models import Movie, Similarity, get_db
 
 if len(sys.argv) != 2:
     print("Usage %s movie_id" % sys.argv[0])
@@ -8,14 +8,16 @@ if len(sys.argv) != 2:
 movie_id = int(sys.argv[1])
 
 db= get_db()
-res=db.execute('select * from similarities where movie_id_1=%i or movie_id_2=%i order by similarity_index desc limit 10' % (movie_id, movie_id))
+res = db.query(Movie).filter_by(movie_id=111351).first()
+print(res.title)
 
 i = 0
-rec_count = 10
+rec_count = 10      
 for r in res:
     i += 1
-    if(r[0]!= 111351):
-        print(r[0],r[2])
+    if(r[0]!= movie_id):
+        m = db.query(Movie).filter_by(movie_id=r[0]).first()
     else:
-        print(r[1],r[2])
+        m = db.query(Movie).filter_by(movie_id=r[1]).first()
 
+    print("Movie: %s similarity: %f" % (m.title, r[2]))

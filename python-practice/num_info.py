790a5630f1180966e334d70efbc7374b9fa79290
Anum Saghir <anumsaghir@gmail.com>
2017-12-23 00:48:21 +0500

    all updates done

---
+++ python-practice/num_info.py
@@ -0,0 +1,20 @@
+import sys
+if len(sys.argv) != 2:
+    print("Usage %i num" % sys.argv[0])
+    sys.exit(1)
+    
+num = int(sys.argv[1])
+if (num % 2) == 0:
+   print("Number", "{0} is Even".format(num))
+else:
+   print("Number", "{0} is Odd".format(num))
+   
+if num > 1:  
+   for i in range(2,num):
+       if (num % i) == 0:
+           print("Number", num,"is not a prime number")
+           break
+   else:
+       print("Number", num,"is a prime number")
+else:
+   print("Number", num,"is not a prime number")

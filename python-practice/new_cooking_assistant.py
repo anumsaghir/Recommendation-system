8a71fbeb460afea245d34d48fdee7f340b0b500f
Anum Saghir <anumsaghir@gmail.com>
2018-01-05 01:11:14 +0500

    not fixed

--- python-practice/new_cooking_assistant.py
+++ python-practice/new_cooking_assistant.py
@@ -16,26 +16,40 @@ def add_recipe():
     recipe_ingredients = input (" Enter Recipe Ingredients: ")
     d['recipe ingredients'] = recipe_ingredients
     return d
-    recipes = d.append()  # Beta return k baad wala code to chalta hn nahi hay, function ki execution return pay khatam ho jati hay
-    # so ye jo oper wali line hay ye to chalni hi nah hay. pluse recipes to global variable hay ap usay function k andar modify nahi
-    # kar sakteen aisay. Additionally, d to dict hay aur append ka method to lists ka hota hay dicts ka nahi.
-    # Aik line aur 3 mistakes :-/
-    
-    
-for menu in main_menu():  # ye main_menu ka function to sirf aik single value return karta hay, loop to lists par lagaya jata hay
-    for choice in menu:   # ye loop is liye chal raha hay k oper menu main bhi sirf aik hi character aya ho ga, is ki zaroorat nahi thi
-        if choice == "a":
-            add_recipe()  # add_recpie ka function kia koi value return nahi karta? dict return karta hay na? to woh ap nay kaheen store karai?
-            print("Recipe Added")
-    
 
-# correct implementation
-user_choice = main_menu()  # just main menu k function ko call kia aur return value store karwa li user_choice main
+def search_recipe_name():
+    search_by_name = input ("Enter Recipe Name: ")
+    for rd in recipes:
+        if rd['recipe name'] == search_by_name:
+            print("Name: " + rd["recipe name"])
+        else:
+            print("Recipe not Found")
+            
+def search_by_ingredients():
+    search_by_ingredients = input ("Enter Recipe ingredients: ")
+    for rd in recipes:
+        if rd['recipe ingredients'] == search_by_ingredients:
+            print("Ingredients: " + rd["recipe ingredients"])
+        else:
+            print("Recipe not Found")
 
-if user_choice == "a":    # compare kar lia k kia choice a hay
-    new_recipe_dict = add_recipe()   # add_recipe ka function call kia aur jo dict us nay return ki woh new_recipe_dict k variable main store kara li
-    recipes.append(new_recipe_dict)  # aur us dict ko jo humari recipes ki list hay us main append kara lia, aisay append karna tha, apko append ka syntax bhool gaya hay :-) jahan koi cheez clear na ho to dobara book ka woh section consult kar lia karain
-
-print(recipes)
+    
+user_choice = main_menu()  
+if user_choice == "a":    
+    new_recipe_dict = add_recipe()   
+    recipes.append(new_recipe_dict)   
+    print("recipe added")
+    
+elif user_choice == "s":
+    search_recipe_name()
+    
+elif user_choice == "i":
+    search_by_ingredients()
+    
+elif user_choice == "q":
+    print("Good bye and happy cooking :-) ")
+else:
+    print("wrong choice")
+    
 
 

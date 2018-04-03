68110bfec895695a6d0a0afcad7ac04388e9303b
Kashif Iftikhar <kashif@compulife.com.pk>
2018-04-03 11:08:18 +0500

    updated t.py with count weightage

--- relational-rtrs/t.py
+++ relational-rtrs/t.py
@@ -10,17 +10,30 @@ def tags_jaccard_index(d1, d2):
     print(set1)
     print(set2)
 
+    common_tags = set.intersection(set1, set2)
+    common_tags_count = 0
+    all_tags_count = sum(d1.values()) + sum(d2.values())
+
+    for k, v in d1.items():
+        if k in common_tags:
+            common_tags_count += v
+
+    for k, v in d2.items():
+        if k in common_tags:
+            common_tags_count += v
+
+    # print(common_tags_count)
+    # print(all_tags_count)
+
     intersection_cardinality = len(set.intersection(set1, set2))
     union_cardinality = len(set.union(set1, set2))
 
     Tag_S = intersection_cardinality/float(union_cardinality)
     
-    print(Tag_S)
+    # print(Tag_S)
     
-    return(Tag_S)
+    final_tag_similarity = ((common_tags_count / all_tags_count) * 10) * Tag_S
+    
+    return(final_tag_similarity)
    
-tags_jaccard_index(d1, d2)
-
-    
-
-
+print(tags_jaccard_index(d1, d2))
